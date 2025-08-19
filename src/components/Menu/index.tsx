b885ccd5b05bbae64a8e4b318dbe6c2a1fdd1481
Pedro Augusto Nunes De Oliveira <panuness1010@gmail.com>
2025-08-19 13:33:48 -0300

    add localstorage theme

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,10 +1,13 @@
 import { HistoryIcon, HomeIcon, MoonIcon, SettingsIcon, SunIcon } from "lucide-react"
 import styles from "./styles.module.css"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 type Themes = 'dark' | 'light'
 
 export function Menu() {
-    const [theme, setTheme] = useState<Themes>('dark')
+    const [theme, setTheme] = useState<Themes>(() => {
+        const storageTheme = localStorage.getItem('theme') as Themes || 'dark'
+        return storageTheme
+    })
 
     function handleThemeChange(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
         event.preventDefault()
@@ -13,16 +16,23 @@ export function Menu() {
             const nextTheme = prevTheme === 'dark' ? 'light' : 'dark'
             return nextTheme
         })
-
     }
+    const nextThemeIcon = {
+        dark: <SunIcon />,
+        light: <MoonIcon />
+    }
+    useEffect(() => {
+        document.documentElement.setAttribute('data-theme', theme)
+        localStorage.setItem('theme', theme)
+
+    }, [theme])
     return (
         <nav className={styles.menu}>
             <a href="" className={styles.menuLink} aria-label="Ir para a home" title="Ir para a home"><HomeIcon /></a>
             <a href="" className={styles.menuLink} aria-label="Mostrar o historico" title="Mostrar o historico"><HistoryIcon /></a>
             <a href="" className={styles.menuLink} aria-label="Configurações" title="Configurações"><SettingsIcon /></a>
             <a href="#" className={styles.menuLink} aria-label="Mudar o tema" title="Mudar o tema" onClick={handleThemeChange}>
-                {theme === 'dark' && <SunIcon />}
-                {theme === 'light' && <MoonIcon />}
+                {nextThemeIcon[theme]}
             </a>
         </nav>
     )
